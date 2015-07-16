84d656db37a492a4d7021c71e319f96aa6bd6366
git config --global user.name Christopher <git config --global user.email nicholsonc@gmail.com>
2015-07-16 19:58:58 -0400

    First test

---
+++ RandomWalk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace RandomWalk
+{
+    class Program
+    {
+      
+
+        static void Main(string[] args)
+        {
+
+            RandomWalk walk = new RandomWalk(35);
+
+            for(int i = 0; i < 1000; i++)
+            {
+                walk.Step();
+            }
+
+          
+
+
+
+
+        }
+    }
+}
---
+++ RandomWalk/RandomWalk.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace RandomWalk
+{
+    class RandomWalk
+    {
+        private static Random rand = new Random();
+        private int[] walkArray;
+        private int currentLocation;
+        private int stepNumber;
+
+        public RandomWalk(int size)
+        {
+            rand = new Random();
+            walkArray = new int[size];
+            currentLocation = walkArray.Length / 2;
+            stepNumber = 1;
+
+            ++walkArray[currentLocation];
+        }
+
+        public void Step()
+        {
+            if (Flip()) currentLocation++;
+            else currentLocation--;
+
+            CheckBounds();
+
+            ++walkArray[currentLocation];
+            stepNumber++;
+
+            PrintVisits();
+
+        }
+
+        private Boolean Flip()
+        {
+            
+            return rand.Next(2) == 0 ? true : false;
+        }
+
+        private void CheckBounds()
+        {
+            if (currentLocation == -1) currentLocation = walkArray.Length - 1;
+            if (currentLocation == walkArray.Length) currentLocation = 0;
+        }
+
+        private void PrintVisits()
+        {
+           Console.Clear();
+
+
+            for (int i = 0; i < walkArray.Length; i++)
+            {
+                Console.Write("{0,2}: ", i);
+
+                for (int j = 0; j < walkArray[i]; j++)
+                {
+                    Console.Write(stepNumber);
+                }
+
+                Console.WriteLine();
+
+               // Thread.Sleep(1);
+            }
+
+        }
+
+    }
+}
